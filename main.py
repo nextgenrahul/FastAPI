bc4c3b4896fa84dcdab0d965c73d32769aef5b15
Rahul Shakya <rahulshakya7685@gmail.com>
2026-05-08 18:29:15 +0530

    Work on FastApi Basics

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 from pydantic import BaseModel
-from typing import List
 
 app = FastAPI()
-
-class Tea(BaseModel):
-    id: int
-    name: str
-    origin: str
+class Post(BaseModel):
+    name : str
+    company : str
+    rating : Optional[int] = None
     
-
-teas: List[Tea] = []
 
 @app.get("/")
 def read_root():
-    return { "message" : "Welcome to my company"}
+    return {"Hello": "World"}
 
-@app.get("/teas")
-def get_teas():
-    return teas
 
-@app.post("/teas")
-def add_tea(tea: Tea):
-    teas.append(tea)
-    return tea
-
-@app.put("/teas/{tea_id}")
-def update_tea(tea_id: int, update_tea: Tea):
-    for index, tea in enumerate(teas):
-        if tea.id == tea_id:
-            teas[index] = update_tea
-            return update_tea
-    return {"error" : "Tea not found"}
-
-@app.delete("/teas/{tea_id}") 
-def delete_tea(tea_id: int):
-    for index, tea in enumerate(teas):
-        if tea.id == tea_id:
-            deleted = teas.pop(index)
-            return deleted
-    return {"error" : "Tea not found"}
+@app.post("/createposts")
+async def create_post(new_post : Post):
+    print(new_post.name)
+    # return {'message' : f"Name : {new_post.name},  Company : {new_post.company}"}
+    return new_post
