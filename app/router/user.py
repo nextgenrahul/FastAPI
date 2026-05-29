9d1dcc0120db9747293a2af666e066c22522c2e5
Rahul Shakya <rahulshakya7685@gmail.com>
2026-05-29 19:09:38 +0530

    Install JWT Library

--- app/router/user.py
+++ app/router/user.py
@@ -38,3 +38,4 @@ def get_user(id: int, db:Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, details=f"User with id : {id} does not exist")
     return user
 
+
